23a9abbfe317914737aa7ddee2aa097f09669cf8
99ShinByeongseon <sun6988shin@naver.com>
2025-01-23 17:47:41 +0900

    🎉 add : 배열의 유사도 추가 가운데 글자 가져오기 수정

--- reactproject/GetMiddle.js
+++ reactproject/GetMiddle.js
@@ -1,5 +1,21 @@
+// 문제 1번
+// 단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 
+// 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+// (제한사항) s는 길이가 1 이상, 100이하인 스트링입니다.
+
+
 function solution(s) {
-    const mid = Math.floor(s.length/2); // 문자열 길이의 절반 계산, 문자열 길이를 2로 나누고 소수점 아래 버리기, 가운데 인덱스 계산
-    return s.length % 2 ? s[mid] : s[mid-1] + s[mid];
-// s.length % 2 홀수 짝수 계산 s[mid] 홀수면 반환(강운데 글자 1개) s[mid-1] + s[mid] 짝수면 (가운데 글자 2개)
+    // 문자열의 길이를 변수에 저장
+    const length = s.length;
+    // 가운데 인덱스 계산
+    const mid = Math.floor(length / 2);
+
+    // 문자열 길이가 홀수인지 짝수인지 조건 분기
+    if (length % 2 === 1) {
+        // 홀수일 경우, 가운데 글자 반환
+        return s[mid];
+    } else {
+        // 짝수일 경우, 가운데 두 글자 반환
+        return s[mid - 1] + s[mid];
+    }
 }
